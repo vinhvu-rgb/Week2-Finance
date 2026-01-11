9d5e3ff359ddf7cc3f72db309fd42de91c1efefe
Vinh Vu <vinhvu619@gmail.com>
2026-01-11 17:07:50 +0000

    Calculate and display daily stock returns

--- scripts/stock_analyzer.py
+++ scripts/stock_analyzer.py
@@ -5,13 +5,18 @@ def main():
     base_dir = os.path.dirname(__file__)
     file_path = os.path.join(base_dir, "..", "data", "sample_stock.csv")
 
+    print("Daily Returns:")
+
     with open(file_path, newline="") as csvfile:
         reader = csv.DictReader(csvfile)
 
-        print("Stock Data:")
         for row in reader:
-            print(row)
+            open_price = float(row["Open"])
+            close_price = float(row["Close"])
+
+            daily_return = (close_price - open_price) / open_price
+
+            print(f"{row['Date']}: {daily_return:.2%}")
 
 if __name__ == "__main__":
     main()
-
